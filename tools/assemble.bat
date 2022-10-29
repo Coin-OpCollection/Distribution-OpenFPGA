eaa2ef67b792aa68493b9ecec550238bf7ab0466
Pramod Somashekar <pramod@pram0d.com>
2022-10-29 13:05:06 -0400

    garegga 10/29 release

--- tools/assemble.bat
+++ tools/assemble.bat
@@ -1,2 +1,5 @@
 set romdir=%1
 mra -z %romdir% -o snowbro2.rom -O "../dist/Assets/snowbros2/common/" "../mra/Snow Bros. 2 - With New Elves - Otenki Paradise (Hanafram).mra"
+mra -z %romdir% -o garegga.rom -O "../dist/Assets/garegga/common/" "../mra/Battle Garegga (Europe - USA - Japan - Asia) (Sat Feb 3 1996).mra"
+mra -z %romdir% -o kingdmgp.rom -O "../dist/Assets/kingdmgp/common/" "../mra/Kingdom Grandprix.mra"
+mra -z %romdir% -o sstriker.rom -O "../dist/Assets/sstriker/common/" "../mra/Sorcer Striker.mra"
