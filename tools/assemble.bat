b0c87622d58c0ea7a6f489ab032c70c7cef262a7
Pramod Somashekar <pramod@pram0d.com>
2022-12-18 12:01:25 -0500

    change set name of batrider to batrideru

--- tools/assemble.bat
+++ tools/assemble.bat
@@ -2,6 +2,6 @@ set romdir=%1
 mra -z %romdir% -o snowbro2.rom -O "../dist/Assets/snowbros2/common/" "../mra/Snow Bros. 2 - With New Elves - Otenki Paradise (Hanafram).mra"
 mra -z %romdir% -o bgaregga.rom -O "../dist/Assets/garegga/common/" "../mra/Battle Garegga (Europe - USA - Japan - Asia) (Sat Feb 3 1996).mra"
 mra -z %romdir% -o bbakraid.rom -O "../dist/Assets/bakraid/common/" "../mra/Battle Bakraid - Unlimited Version (USA) (Tue Jun 8 1999).mra"
-mra -z %romdir% -o batrider.rom -O "../dist/Assets/batrider/common/" "../mra/Armed Police Batrider (USA) (Fri Feb 13 1998).mra"
+mra -z %romdir% -o batrideru.rom -O "../dist/Assets/batrider/common/" "../mra/Armed Police Batrider (USA) (Fri Feb 13 1998).mra"
 mra -z %romdir% -o kingdmgp.rom -O "../dist/Assets/kingdmgp/common/" "../mra/Kingdom Grandprix.mra"
 mra -z %romdir% -o sstriker.rom -O "../dist/Assets/sstriker/common/" "../mra/Sorcer Striker.mra"
